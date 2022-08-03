b7928f6742f98e3573a21b2d65a8f2dc5d5ef1a3
정우철 <oocheol@naver.com>
2022-08-03 17:37:42 +0900

    불필요페이지 삭제

--- src/main/java/com/smart/project/web/login/act/LoginUserAction.java
+++
@@ -1,8 +0,0 @@
-package com.smart.project.web.login.act;
-
-import org.springframework.stereotype.Controller;
-
-@Controller
-public class LoginUserAction {
-
-}
--- src/main/java/com/smart/project/web/login/vo/LoginVO.java
+++
@@ -1,7 +0,0 @@
-package com.smart.project.web.login.vo;
-
-import lombok.Data;
-
-@Data
-public class LoginVO {
-}
